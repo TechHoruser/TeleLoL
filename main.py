24134f696ba2ec637eec733fcc9e033e0d801b46
Unknown <pon_ton_ce@hotmail.com>
2017-06-07 23:55:00 +0200

    Añadido uso telegram y comando /chest

--- main.py
+++ main.py
@@ -1,10 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from config.Params import Params
 from src.Riot import Riot
 
-from config.Params import Params
+import telebot
+import os
+from telebot import types
 
-riot = Riot( Params.key, 'Horuser' )
+tb = telebot.TeleBot( Params.token_telegram ) 
+riot = Riot( Params.key, Params.tmp_folder )
 
-riot.getSummonerInfo()
+@tb.message_handler(commands=['start'])
+def handle_start_help(message):
+	mensaje = '<b>Bienvenido al canal League of Laughs, esperemos que te sea de utilidad ;)</b>'
+	tb.send_message( message.chat.id, mensaje, parse_mode = 'HTML' )
+
+@tb.callback_query_handler(func=lambda call: True)
+def test_callback(call):
+	global participaciones, premiado, usuario_premiado
+
+	if call.from_user.username == usuario_premiado:
+		premiado = True
+		tb.send_message( call.from_user.id, '<b>Felicidades has sido premiad@</b>'+os.linesep +'Escribanos su email para ponernos en contacto con usted en las próximas 48h.', parse_mode = 'HTML' )
+	else:
+		if premiado == False:
+			if call.from_user.id in participaciones:
+				tb.send_message( call.from_user.id, 'Ya ha realizado la participación en el sorteo, vuelva a intentarlo en el próximo.' )
+			else:
+				participaciones.append(call.from_user.id)
+				tb.send_message( call.from_user.id, 'Lo sentimos no ha tenido suerte, vuelve a intentarlo en el próximo sorteo.' )
+		else:
+			tb.send_message( call.from_user.id, 'Ya tenemos ganador, vuelva a intentarlo en el próximo sorteo.' )
+
+@tb.message_handler(commands=['summoner'])
+def handle_text_doc(message):
+	summoner = message.text.split(' ', 1)[1]
+	reponse_riot = riot.getSummonerInfo( summoner )
+	# print( message.from_user.id+': '+message.text )
+	# print( json_dumps( message.from_user ) )
+	tb.send_message( message.from_user.id, reponse_riot )
+
+@tb.message_handler(commands=['chest'])
+def handle_text_doc(message):
+	if len( message.text.split(' ') ) == 3:
+		mensaje = message.text.split(' ', 1)[1]
+		summoner_name = mensaje.split(' ', 1)[0]
+		champ_name = mensaje.split(' ', 1)[1]
+
+		reponse_riot = riot.getHaveChest( summoner_name, champ_name )
+
+		tb.send_message( message.from_user.id, reponse_riot )
+	else:
+		tb.send_message( message.from_user.id, 'Mala solicitud' )
+
+
+tb.polling(none_stop=True)
--- src/Riot.py
+++ src/Riot.py
@@ -6,41 +6,58 @@ import json
 
 
 class Riot(object):
-	def __init__( self, key, summoner_name ):
+	def __init__( self, key, tmp_folder ):
 		super( Riot, self ).__init__()
 		self.key = key
-		self.summoner_name = summoner_name
 		self.headers = {
 		    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-		    "X-Riot-Token": "5e9e5832-1ccd-43f0-97aa-2a26c9071c2b",
+		    "X-Riot-Token": key,
 		    "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
 		    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0"
 		     }
+		self.tmp_folder = tmp_folder
 
-	def getSummonerInfo( self ):
+	def getSummonerInfo( self, summoner_name ):
 		try:
 			# Obtain summoner's info
-			url_summoner = 'https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + self.summoner_name
+			url_summoner = 'https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + summoner_name
 			summoner_response = self.getResponse( url_summoner )
 
-			print 'Info de Invocador:\n'
-			print summoner_response
+			open( self.tmp_folder+'summoner.json', 'w').write(  json.dumps( summoner_response ) )
 
 			try:
 				# Obtain current game info
 				url_current_game = 'https://euw1.api.riotgames.com/observer-mode/rest/consumer/getSpectatorGameInfo/EUW1/' + str( summoner_response['id'] )
 				current_game_response = self.getResponse( url_current_game )
 
-				print 'Info de Partida Actual:\n'
-				print json.dumps(current_game_response)
+				game = {
+					'allies' : {},
+					'enemies' : {}
+				}
 
-				print '\n\n'
+				print current_game_response[ "gameMode" ]
+				
+				first_allies = False
+				if ( current_game_response[ "gameMode" ] == 'ARAM' or current_game_response[ "gameMode" ] == 'CLASSIC' ):
+					tam_team = 5
+				elif ( current_game_response[ "gameMode" ] == 'ARAM' ):
+					tam_team = 3
+
+				for index in range( 0, tam_team*2 ):
+					game[ 'allies' if ( ( first_allies and index < tam_team ) or ( not first_allies and index >= tam_team ) ) else 'enemies' ][ current_game_response[ 'participants' ][ index ][ 'summonerId' ] ] = {
+						'name' : current_game_response[ 'participants' ][ index ][ 'summonerName' ],
+						'champion' : current_game_response[ 'participants' ][ index ][ 'championId' ]
+					}
+				
+				# open( self.tmp_folder+'game.json', 'w').write(  json.dumps( game ) )
+				return json.dumps( game )
+				# open( self.tmp_folder+'current_game.json', 'w').write(  json.dumps( current_game_response ) )
 
 			except urllib2.HTTPError as err:
-				print 'Error al encontrar partida'
+				return 'Error al encontrar partida'
 
 		except urllib2.HTTPError as err:
-			print 'Invocador no encontrado'
+			return 'Invocador ' + summoner_name + ' no encontrado'
 
 
 	def getResponse( self, url ):
@@ -49,3 +66,43 @@ class Riot(object):
 		response = response.read()
 
 		return json.loads( response )
+
+
+	def getHaveChest( self, summoner_name, champ_name ):
+		url = 'https://euw1.api.riotgames.com/lol/static-data/v3/champions?champListData=tags&tags=tags&dataById=true'
+		request  = urllib2.Request( url, headers = self.headers )
+		response = urllib2.urlopen( request )
+		response = response.read()
+
+		json_response = json.loads( response )
+
+		json_response = json_response[ 'data' ]
+
+		chmp_idx = 0
+
+		for idx, champ in json_response.iteritems(): # Iterador JSON
+			if champ_name == champ[ 'name' ]:
+				chmp_idx = idx
+
+
+		url = 'https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + summoner_name
+		request  = urllib2.Request( url, headers = self.headers )
+		response = urllib2.urlopen( request )
+		response = response.read()
+
+		json_response = json.loads( response )
+
+		summoner_id = json_response[ 'id' ]
+
+		url = 'https://euw1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/' + (str)(summoner_id)
+		request  = urllib2.Request( url, headers = self.headers )
+		response = urllib2.urlopen( request )
+		response = response.read()
+
+		json_response = json.loads( response )
+
+		for champ in json_response:
+			if champ[ 'championId' ] == (int)(chmp_idx):
+				return ( 'Caja conseguita' if champ[ 'chestGranted' ] else 'Caja no conseguita' )
+
+		return 'No dispones del campeón'
